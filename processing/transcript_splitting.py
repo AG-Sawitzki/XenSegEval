d368a01bc480213f4a5b30f1d9b35d937ca944db
Julius Normann <juno12_c@hpc-cpu-199-private.cubi.bihealth.org>
2026-05-08 12:40:53 +0200

    style and change sample

--- processing/transcript_splitting.py
+++ processing/transcript_splitting.py
@@ -6,12 +6,13 @@ Transcripts are saved as csv.gz | relative micrometer coordinates
 
 from pathlib import Path
 import configparser
+import functools
 import argparse
 
 import json
 import gzip
 
-from numpy.lib.stride_tricks import sliding_window_view
+# from numpy.lib.stride_tricks import sliding_window_view
 import multiprocessing as mp
 import pandas as pd
 import numpy as np
@@ -23,7 +24,7 @@ import numpy as np
 #     Args:
 #         bins:    the bins...
 #         section:    string of the section number.
-    
+
 #     Returns:
 #         Strings for the bins.
 #     """
@@ -33,7 +34,7 @@ import numpy as np
 #     x_bins_v = sliding_window_view(x_bins, (2,))
 
 #     coord = np.array(sections_dict[section])*pixelsize
-    
+
 #         # check in which bin
 #     y_bin_index = np.where(y_bins_v <= coord[:,0])[0][-1]
 #     x_bin_index = np.where(x_bins_v <= coord[:,1])[0][-1]
@@ -51,7 +52,7 @@ import numpy as np
 #     Returns:
 #         List of two arrays for the bins. [y_bin, x_bin]
 #     """
-#     keys = sections_dict.keys()    
+#     keys = sections_dict.keys()
 #     values = np.array([sections_dict[i] for i in keys])*pixelsize
 
 #     y_min, x_min = np.min(values, axis=0)[0]
@@ -67,7 +68,7 @@ import numpy as np
 #             continue
 #         else:
 #             print(f'Bin too small for section {i}')
-    
+
 #     return [y_bins, x_bins]
 
 def regions_to_extract(sections_dict, pixelsizeXY):
@@ -91,7 +92,7 @@ def regions_to_extract(sections_dict, pixelsizeXY):
             'y_max': y_max_px * pixelsizeXY,
             'x_max': x_max_px * pixelsizeXY,
         }
-    
+
     return regions
 
 def relative(df, region_data):
@@ -106,7 +107,7 @@ def relative(df, region_data):
     df['y_location'] = (df['y_location'] - region_data['y_min'])
     df['x_location'] = (df['x_location'] - region_data['x_min'])
     # df.loc[:,'x_location':'y_location'] = (df.loc[:,'x_location':'y_location'].to_numpy() - sections_dict[section][0]).round(0).astype(np.int64)
-    
+
     return df
 
 def pixelate(df, pixelsize):
@@ -119,17 +120,22 @@ def pixelate(df, pixelsize):
     Returns:
         A pixelated dataframe in x,y and z.
     """
-    df['y_location'] = (df['y_location'] 
-                        / pixelsize[0]).round(0).astype(np.int64)
-    df['x_location'] = (df['x_location'] 
-                        / pixelsize[0]).round(0).astype(np.int64)
-	df['z_location'] = (df['z_location'] 
-                        / pixelsize[1]).round(0).astype(np.int64)
+    df['y_location'] = (
+        df['y_location'] / pixelsize[0]
+    ).round(0).astype(np.int64)
+
+    df['x_location'] = (
+        df['x_location'] / pixelsize[0]
+    ).round(0).astype(np.int64)
+
+    df['z_location'] = (
+        df['z_location'] / pixelsize[1]
+    ).round(0).astype(np.int64)
 
     # arr = (df.loc[:,'x_location':'y_location'].to_numpy() / pixelsize).round(0).astype(np.int64)
 	# arr.round(0)
 	# df.loc[:,'x_location':'y_location'] = arr.astype(np.int64)
-	
+
     return df
 
 def process_chunk(df):
@@ -156,27 +162,27 @@ def process_chunk(df):
             & (y_min <= df['y_location'])
             & (df['y_location'] <= y_max)
         ] = region_name
-    
+
     df['region'] = regions_mapping
 
     # y_binned = pd.cut(df['y_location'], bins[0], labels = ['y0', 'y1', 'y2', 'y3'], include_lowest = True).to_numpy()
     # x_binned = pd.cut(df['x_location'], bins[1], labels = ['x0', 'x1', 'x2'], include_lowest = True).to_numpy()
-    
+
     # index_ = df.index.to_numpy()
     # index = pd.MultiIndex.from_arrays([y_binned, x_binned, index_], names = ('y_bin', 'x_bin', 'idx'))
-    
+
     # df.index = index
-    
+
     # df.reset_index(inplace = True)
     # df.dropna(axis = 0, inplace = True)
     # df.set_index(['y_bin', 'x_bin', 'idx'], inplace = True)
     # df.sort_index(inplace = True)
-    
+
     # if df.index.is_monotonic_increasing != True:
     #     # print('sorted')
     #     # else:
     #     print('sorting already prob here')
-    
+
     return df
 
 # def process_SubFrame(df, section, sections_dict):
@@ -184,12 +190,12 @@ def process_chunk(df):
 #     Processes the SubFrame (metabin extract of the DataFrame).
 #         - Pixelates it.
 #         - Assigns new bins based on upper left and lower right corner
-    
+
 #     Args:
 #         df: a DataFrame
 #         section: string of the section
 #         sections_dict: dictionary of section coordinates
-    
+
 #     Returns:
 #         DataFrame with values between y/x min/max.
 #     """
@@ -197,7 +203,7 @@ def process_chunk(df):
 #     df = pixelate(df)
 #     print(df.loc[:10,'x_location':'y_location'])
 #     print('px')
-    
+
 #     y_min, x_min = sections_dict[section][0]
 #     y_max, x_max = sections_dict[section][1]
 #     print('bins2')
@@ -213,7 +219,7 @@ def process_chunk(df):
 #     df.sort_index(inplace=True)
 #     print('indexed')
 #     print(df.head(n=5))
-    
+
 #     df = df.loc[('y','x')]
 #     print('masked')
 
@@ -241,16 +247,17 @@ def save_section(df, region_name, regions):
 
     sub_results_df = df[df['regions'] == region_name]
     print(sub_results_df.loc[:10,'x_location':'y_location'])
-    
+
     # px_filtered_sub_results_df = process_SubFrame(sub_results_df, section, sections_dict)
     # print('masked and px')
     # relative_px_filtered_sub_results_df = relative(px_filtered_sub_results_df, sections_dict)
     # print('relative')
-    
+
     sub_results_df = relative(sub_results_df, region_data)
 
-    sub_results_df = pixelate(sub_results_df, 
-                              pixelsize=(pixelsizeXY, pixelsizeZ)
+    sub_results_df = pixelate(
+        sub_results_df,
+        pixelsize=(pixelsizeXY, pixelsizeZ)
     )
 
     sub_results_df.drop(columns='regions', inplace=True)
@@ -261,10 +268,10 @@ def save_section(df, region_name, regions):
         # save thingy
         output_dir = Path(processed / '{0}/transcripts/'.format(region_name))
         output_dir.mkdir(parents=True, exist_ok=True)
-        
+
         sub_results_df.to_csv(csv_path / 'relative.csv', index=False)
-        
-        # compress for ProSeg        
+
+        # compress for ProSeg
         sub_results_df.to_csv(output_dir / 'relative.csv.gz', index=False,
                               compression='infer'
         )
@@ -272,7 +279,7 @@ def save_section(df, region_name, regions):
         print(f'region {region_name}: saved restults')
 
 if __name__ == '__main__':
-    
+
     # define paths
     parser = argparse.ArgumentParser(prog='Image Processing.')
     parser.add_argument('-c', '--Config', help='Path to the config file.')
@@ -314,7 +321,7 @@ if __name__ == '__main__':
     print('Processing Chunks.')
     with mp.Pool(processes=mp.cpu_count()-1) as pool:
         with pd.read_csv(
-            data / 'transcripts.csv.gz', 
+            data / 'transcripts.csv.gz',
             compression = 'infer',
             dtype=dtype_dict,
             chunksize = 20000
@@ -323,7 +330,7 @@ if __name__ == '__main__':
                 functools.partial(
                     process_chunk,
                     regions=regions
-                )
+                ),
                     reader
             ) # chunksize = int(20000/mp.cpu_count()-1))
             pool.close()
@@ -331,7 +338,7 @@ if __name__ == '__main__':
             results_df = pd.concat(results)
     # results_df.index = results_df.index.sort_values()
     print(results_df.head(n=5))
-    
+
     # save the sections
     with mp.Pool(processes=mp.cpu_count()-1) as pool:
         pool.imap_unordered(
