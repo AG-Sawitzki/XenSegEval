fb70b7b3ef55fd5d524250eb75937e66944e0622
Minna <jc.normann@outlook.com>
2026-05-22 12:36:37 +0200

    stardist works

--- segmenting/seg_stardist.py
+++ segmenting/seg_stardist.py
@@ -11,29 +11,47 @@ import tomllib
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='Image Processing.')
-    parser.add_argument('-c', '--Config', help='Path to the config file.')
+    parser.add_argument('-c', '--Config',
+        help='Path to the config file.'
+    )
+    parser.add_argument('-s', '--Section',
+        help='Number of sample-section to segment. Or "all".'
+    )
     args = parser.parse_args()
 
     config_path = args.Config
-
+    sections = args.Section
+    try:
+        sections = [int(sections)]
+    except:
+        sections = range(12)
+    
     with open(config_path, 'rb') as f:
         config = tomllib.load(f)
 
     home = config['paths']['home']
     data = Path(config['paths']['data_path'])
     sample = config['paths']['name']
+    planes = config['processing']['planes']
 
     processed = Path(f'{home}{sample}/processed')
     processed.mkdir(parents=True, exist_ok=True)
-    path = Path('/data/cephfs-2/unmirrored/groups/sawitzki/Juno/processed/data_processed/image-data_processed/morphology/36/quatered')
 
+    single_layer = Path(processed / 'morphology/single_layer/')
 
     # creates a pretrained model
     model = StarDist2D.from_pretrained('2D_versatile_fluo')
 
-    for i in range(4):
-        img = imread(path / 'morphology_L6-7_q0{0}.tif'.format(i))
+    for section in sections:
+        for l, layer in enumerate(single_layer.glob('*0*')):
+            l = planes[l]
+            for q, quater in enumerate(layer.glob('quatered/q0*.tif')):
 
-        labels, _ = model.predict_instances(normalize(img))
-        
-        np.save(f'{sample}...results/stardist/36_q0{0}.npy'.format(i), labels)
+                img = imread(quater)
+
+                labels, _ = model.predict_instances(normalize(img))
+
+                np.save(
+                    f'{sample}/results/stardist/{section}/layer0{l}/q0{q}.npy',
+                    labels
+                )
