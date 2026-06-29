3fb92717c7aa4a69fd31c0348c988cf48b9485e9
juno12 <>
2026-06-29 12:21:59 +0200

    WIP ucs | rm ucs from toml | add ucs scripts

--- XenSegEval/start/ucs.sh
+++ XenSegEval/start/ucs.sh
@@ -6,16 +6,18 @@ SECTION=$2
 home=$(get_toml_value "$CONFIG_FILE" "paths" "home")
 sample=$(get_toml_value "$CONFIG_FILE" "paths" "sample_name")
 n_roi=$(get_toml_value "$CONFIG_FILE" "preprocessing" "n_roi")
-sections_path=$(get_toml_value "$CONFIG_FILE" "paths" "sections")
+sections_path=$(get_toml_value "$CONFIG_FILE" "paths" "sections_path")
 CHECK_GENE_MAP=$(get_toml_value "$CONFIG_FILE" "methods.ucs" "check_gene_map")
 #
 . ~/.bashrc
 sections=$(jq 'keys' $sections_path | jq .[] | tr -d ' "')
-pixi shell -e ucs
+#pixi shell -e ucs
 #
-for s in ${}; do
+echo $sections
+for s in ${sections}; do
+    echo $s
     # Run the preprocess to get the gene map and official nuclei mask
-    python preprocess/xenium.py \
+    python -m XenSegEval.segmenting.ucs.xenium \
     --transcripts $home/$sample/processed/$s/transcripts/relative.parquet \ 
     --cell_boundary_10X $home/$sample/processed/boundaries/cell_relative.parquet \
     --nucleus_boundary_10X $home/$sample/processed/boundaries/nucleus_relative.parquet \
@@ -31,7 +33,7 @@ for s in ${}; do
     fi
     #
     # Run UCS
-    python run.py \
+    python -m run \
     --gene_map $home/$sample/results/ucs/$s/gene_map.tif \
     --nuclei_mask $home/$sample/results/ucs/$s/nuclei_10X_mask.tif \
     --log_dir $home/$sample/run/ucs/log/$s/
